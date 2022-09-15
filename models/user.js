a00f6fc88442fb0514ae47244de6d3e9ac4a7024
Shaun <scpour@gmail.com>
2022-09-15 10:17:18 -0400

    Passport Facebook

--- models/user.js
+++ models/user.js
@@ -3,14 +3,18 @@ const passportLocalMongoose = require('passport-local-mongoose');
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
+    facebookId: String,
+
     firstname: {
         type: String,
         default: ''
     },
+
     lastname: {
         type: String,
         default: ''
     },
+
     admin: {
         type: Boolean,
         default: false
